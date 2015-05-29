81700de8dde235e157516931bdc1604e21800ed0
Gonzalo Ramos <gonzalo.ramos@beeva.com>
2015-05-29 17:37:58 +0200

    Initial

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
+import PIL.Image
+import PIL.ImageDraw
+import PIL.ImageFont
 from Marcador import Marcador
 
 marcador = Marcador("Enemy Crushers", "Orangutanes")
 
+img = PIL.Image.new('RGBA', (800, 400), (255, 0, 0, 0))
+font = PIL.ImageFont.truetype("AbyssinicaSIL-R.ttf", 32)
+
+d = PIL.ImageDraw.Draw(img)
+d.text((180, 20), marcador.teamios[0]+ ' ' + str(marcador.teamios[1]) + '-' +
+       str(marcador.teamandroid[1]) + ' ' + marcador.teamandroid[0],
+       fill=(255, 255, 255), font=font)
 
 
+img.save('/tmp/img.png', 'PNG')
